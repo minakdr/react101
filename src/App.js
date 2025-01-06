19c48cf0788dd84c6112fca25c8831c3804d4e31
minakdr <mina125.ak@gmail.com>
2025-01-06 11:34:31 +0100

    we're done for now

--- src/App.js
+++ src/App.js
@@ -4,21 +4,23 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+<main>
+
+<header>
+<header>
+  <img src={logo} width="40px" alt="React logo" />
+</header>
+            </header>
+                <h1>Reason I am excited to learn React</h1>
+                <ol>
+                    <li>React is a popular library, so I will be able to fit in with all the coolest devs out there! 😎</li>
+                    <li>I am more likely to get a job as a front end developer if I know React</li>
+                </ol>
+            </main>
+            <footer>
+                <small>© 2024 Mina development. All rights reserved.</small>
+            </footer>
+        </div>
   );
 }
 
